a5f1e5418fa31ec9f73bd39fd25198e07637a072
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-21 22:27:39 +0300

    task 003 with string

---
+++ Task003_2_define_number_of_digits/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Input positive integer and press Enter: ");
+int a = int.Parse(Console.ReadLine());
+string c;
+c = a.ToString();
+int number = c.Length;
+Console.Write($"The number of digits in {a} = {number}.");
