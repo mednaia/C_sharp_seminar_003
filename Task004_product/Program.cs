ff1fed87033eef05022fc2e3815b737e3512cc62
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-21 22:53:38 +0300

    task 004 product

--- Task004_product/Program.cs
+++ Task004_product/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Написать программу вычисления произведения чисел от 1 до N
 Console.WriteLine("Input positive integer and press Enter: ");
 int n = int.Parse(Console.ReadLine());
-int product = 1;
+double product = 1;
 for (int count = 1; count<=n; count++)
 {
     product = product * count;
