5562bb59ef282160e208935ad782353037c1956a
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-21 22:13:27 +0300

    Task 003 to define numbers of digit in N

---
+++ Task003_define_number_of_digits/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input positive integer and press Enter: ");
+int a = int.Parse(Console.ReadLine());
+int number = 0;
+int temp = a;
+while (temp > 0)
+{
+    temp = temp/10;
+    number++;
+}
+Console.Write($"The number of digits in {a} = {number}.");
