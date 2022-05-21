0bd71d083b846845e60ebccc86d586ece94381ea
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-21 12:06:08 +0300

    Task 001 square from 1 to N

---
+++ Task001_square_from_1_to_N/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input  positive integer and press Enter: ");
+int n = int.Parse(Console.ReadLine());
+int count = 1;
+while(count <= n)
+{
+    int result = count * count;
+    Console.WriteLine(result);
+    count++;
+}
