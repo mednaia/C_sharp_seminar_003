daafa60b731690ab067ac797101e5fb6c0a5cc67
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-21 16:26:54 +0300

    Task002 sum from 1 to A

---
+++ Task002_sum_from_1_to_A/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input  positive integer and press Enter: ");
+int a = int.Parse(Console.ReadLine());
+int count = 1;
+int sum = 0;
+while (count <= a)
+{
+    sum = sum + count;
+    count++;
+}
+Console.Write($"Sum of numbers from 1 to {a} = {sum}.");
