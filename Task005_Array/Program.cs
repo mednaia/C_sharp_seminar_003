1f876e8ccc4e416ea03dc32749c8436d15b3927a
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-22 00:10:11 +0300

    Task005 Array

---
+++ Task005_Array/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+int[] a=new int[8];
+Random rand = new Random();
+for (int i=0; i <8; i++)
+{
+    a[i] = rand.Next(1,10);
+    Console.Write(" " + a[i]);
+}
+   
